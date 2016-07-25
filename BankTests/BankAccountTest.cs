93cb6ea63d9057110d67bef6f672e851a010a796
Winter SWeet <ge_zhang@hotmail.com>
2016-07-25 09:12:40 -0700

    An old C# unit test before

---
+++ BankTests/BankAccountTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using BankAccountNS;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTests
+{
+    [TestClass]
+    public class  ankAccountTest
+    {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //}
+
+        // unit test code
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_shouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 15.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
